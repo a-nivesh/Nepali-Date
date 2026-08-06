0e145f8add367f374391d77270b9244245e1a94e
a-nivesh <41886341+a-nivesh@users.noreply.github.com>
2026-08-06 15:20:25 +0545

    Fix fiscal year calculation logic in DateFormatter

--- src/Utils/DateFormatter.php
+++ src/Utils/DateFormatter.php
@@ -168,7 +168,7 @@ class DateFormatter {
 
     private function resolveFiscalYear($nepali = false)
     {
-        $year = $this->month > 3 ? $this->year : $this->year - 1;
+        $year = $this->month > 2 ? $this->year : $this->year - 1;
         $fiscalYear = $this->getFiscalYearMapping()[$year];
 
         return $nepali ? $this->convertNumber($fiscalYear) : $fiscalYear;
